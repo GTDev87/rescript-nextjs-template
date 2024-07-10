68cbe62e4db85c0882c491a991a1e9cc717dca33
Gregory Thompson <gregorythompson@Gregorys-MBP.lan>
2024-07-10 13:16:34 -0400

    updated to version 20, rescript 0.12.2, rescript to 11.1.2, react to 18.3.1

--- src/App.mjs
+++ src/App.mjs
@@ -3,27 +3,32 @@
 import * as React from "react";
 import * as MainLayout from "./layouts/MainLayout.mjs";
 import * as Router from "next/router";
+import * as JsxRuntime from "react/jsx-runtime";
 
 function $$default(props) {
   var router = Router.useRouter();
   var content = React.createElement(props.Component, props.pageProps);
   var match = router.route;
   if (match === "/examples") {
-    return React.createElement(MainLayout.make, {
-                children: null
-              }, React.createElement("h1", {
-                    className: "font-bold"
-                  }, "Examples Section"), React.createElement("div", undefined, content));
+    return JsxRuntime.jsxs(MainLayout.make, {
+                children: [
+                  JsxRuntime.jsx("h1", {
+                        children: "Examples Section",
+                        className: "font-bold"
+                      }),
+                  JsxRuntime.jsx("div", {
+                        children: content
+                      })
+                ]
+              });
   } else {
-    return React.createElement(MainLayout.make, {
+    return JsxRuntime.jsx(MainLayout.make, {
                 children: content
               });
   }
 }
 
 export {
-  $$default ,
   $$default as default,
-  
 }
 /* react Not a pure module */

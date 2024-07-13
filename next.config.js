4ac1e1108ed365c4896551b211318f2bacd477d1
Gregory Thompson <gregorythompson@MacBook-Pro.lan>
2024-07-12 22:57:45 -0400

    removes webpack5 config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const transpileModules = ["rescript"].concat(rescript["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
 
-const isWebpack5 = true;
 const config = {
   // target: "serverless",
   // swcMinify: true,
@@ -15,28 +14,26 @@ const config = {
   webpack: (config, options) => {
     const { isServer } = options;
 
-    if (isWebpack5) {
-      if (!isServer) {
-        // We shim fs for things like the blog slugs component
-        // where we need fs access in the server-side part
-        config.resolve.fallback = {
-          fs: false,
-          path: false,
-        };
-      }
-
-      // We need this additional rule to make sure that mjs files are
-      // correctly detected within our src/ folder
-      config.module.rules.push({
-        test: /\.m?js$/,
-        use: [options.defaultLoaders.babel, 'astroturf/loader'],
-        exclude: /node_modules/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        }
-      });
+    if (!isServer) {
+      // We shim fs for things like the blog slugs component
+      // where we need fs access in the server-side part
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
     }
+
+    // We need this additional rule to make sure that mjs files are
+    // correctly detected within our src/ folder
+    config.module.rules.push({
+      test: /\.m?js$/,
+      use: [options.defaultLoaders.babel, 'astroturf/loader'],
+      exclude: /node_modules/,
+      type: "javascript/auto",
+      resolve: {
+        fullySpecified: false,
+      }
+    });
     return config
   },
 };
